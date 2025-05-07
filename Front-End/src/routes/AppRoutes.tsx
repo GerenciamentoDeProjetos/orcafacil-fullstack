90631bcee03c128aab7e8d705e6dfe2e4861d35a
RobertoLimaVieira <robertinvieira6@gmail.com>
2025-05-06 21:34:31 -0300

    Dashboard page frontend

--- Front-End/src/routes/AppRoutes.tsx
+++ Front-End/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/login/LoginPage';
 import RegisterPage from '../pages/register/RegisterPage';
 import ForgotPasswordPage from '../pages/forgotpassword/ForgotPasswordPage';
+import Dashboard from '../pages/dashboard/Dashboard';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
